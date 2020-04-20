ab9ec38cb849b51acabc37f08de38674d3225a7c
M1057704 <Pavan.S2@mindtree.com>
2020-04-21 00:37:05 +0530

    inital commit

---
+++ src/main/java/com/mindtree/movieBooking/controller/BookingPortalController.java
@@ -0,0 +1,85 @@
+package com.mindtree.movieBooking.controller;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mindtree.movieBooking.dto.BookingPortalDTO;
+import com.mindtree.movieBooking.dto.ResponseBody;
+import com.mindtree.movieBooking.entity.BookingPortal;
+import com.mindtree.movieBooking.exception.MovieBookingAppException;
+
+import com.mindtree.movieBooking.service.BookingPortalService;
+
+@RestController
+public class BookingPortalController {
+	
+	@Autowired
+	private BookingPortalService bookingPortalService;
+
+	@Autowired
+	private ModelMapper modelMapper;
+
+	@PostMapping("/bookingportal")
+	public ResponseEntity<?> addBookingPortal(@Valid @RequestBody BookingPortalDTO bookingPortal)
+			throws MovieBookingAppException {
+
+		return new ResponseEntity<ResponseBody<BookingPortalDTO>>(
+				new ResponseBody<BookingPortalDTO>(
+						modelMapper.map(bookingPortalService.addBookingPortalData(
+								modelMapper.map(bookingPortal, BookingPortal.class)), BookingPortalDTO.class),
+						null, "Booking Portal Added Sucessfully", true),
+				HttpStatus.OK);
+
+	}
+
+	@GetMapping("/bookingportal")
+	public ResponseEntity<?> getAllBookingPortal() throws MovieBookingAppException {
+
+		return new ResponseEntity<ResponseBody<List<BookingPortalDTO>>>(new ResponseBody<List<BookingPortalDTO>>(
+				modelMapper.map(bookingPortalService.getBookingPortalData(), new TypeToken<List<BookingPortalDTO>>() {
+				}.getType()), null, "Bokking Portal List Found", true), HttpStatus.OK);
+
+	}
+
+	@DeleteMapping("/bookingportal/{portalName}")
+	public ResponseEntity<?> deleteBookingPortal(@PathVariable String portalName) throws MovieBookingAppException {
+
+		bookingPortalService.deleteBookingPorttalData(portalName);
+		return new ResponseEntity<ResponseBody<Void>>(
+				new ResponseBody<Void>(null, null, "Booking Porttal with name " + portalName + " is deleted. ", true),
+				HttpStatus.OK);
+
+	}
+	
+	@GetMapping("/serialization")
+	public ResponseEntity<?> getBookingPortalDetails() throws MovieBookingAppException {
+
+		return new ResponseEntity<ResponseBody<Set<BookingPortalDTO>>>(new ResponseBody<Set<BookingPortalDTO>>(
+				modelMapper.map(bookingPortalService.getBookingPortalDetails(), new TypeToken<Set<BookingPortalDTO>>() {
+				}.getType()), null, "Booking Portal detalis are serialized", true), HttpStatus.OK);
+	}
+	
+	@GetMapping("/deserialization")
+	public ResponseEntity<?> getBookingPortalDetail() throws MovieBookingAppException {
+
+		return new ResponseEntity<ResponseBody<Set<BookingPortalDTO>>>(new ResponseBody<Set<BookingPortalDTO>>(
+				modelMapper.map(bookingPortalService.getBookingPortalDetail(), new TypeToken<Set<BookingPortalDTO>>() {
+				}.getType()), null, "Booking Portal details are deserialized", true), HttpStatus.OK);
+	}
+	
+
+}
---
+++ src/main/java/com/mindtree/movieBooking/controller/MovieTicketController.java
@@ -0,0 +1,68 @@
+package com.mindtree.movieBooking.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mindtree.movieBooking.dto.MovieTicketDTO;
+import com.mindtree.movieBooking.dto.ResponseBody;
+import com.mindtree.movieBooking.entity.MovieTicket;
+import com.mindtree.movieBooking.exception.MovieBookingAppException;
+import com.mindtree.movieBooking.service.MovieTicketService;
+
+@RestController
+public class MovieTicketController {
+
+	@Autowired
+	private MovieTicketService movieTicketService;
+
+	@Autowired
+	private ModelMapper modelMapper;
+
+	@PostMapping("/movieticket/{portalName}")
+	public ResponseEntity<?> addMovieTicket(@Valid @RequestBody MovieTicketDTO movieTicket,
+			@PathVariable String portalName) throws MovieBookingAppException {
+
+		return new ResponseEntity<ResponseBody<MovieTicketDTO>>(
+				new ResponseBody<MovieTicketDTO>(
+						modelMapper.map(movieTicketService.addMovieTicketData(
+								modelMapper.map(movieTicket, MovieTicket.class), portalName), MovieTicketDTO.class),
+						null, "Movie ticket is booked Successfully", true),
+				HttpStatus.OK);
+
+	}
+
+	@DeleteMapping("/movietickte/{movieName}")
+	public ResponseEntity<?> deleteMovie(@PathVariable String movieName) throws MovieBookingAppException {
+		movieTicketService.deleteMovieData(movieName);
+		return new ResponseEntity<ResponseBody<Void>>(
+				new ResponseBody<Void>(null, null, "Movie " + movieName + " is Deleted.", true), HttpStatus.OK);
+
+	}
+
+	@GetMapping("/movieticket/writinginexcel")
+	public ResponseEntity<?> writingIntoExcel() throws MovieBookingAppException {
+		movieTicketService.writingIntoExcel();
+		return new ResponseEntity<ResponseBody<Void>>(
+				new ResponseBody<Void>(null, null, "writing into excel is done", true), HttpStatus.OK);
+	}
+
+	@GetMapping("/movieticket/readingfromexcel")
+	public ResponseEntity<?> readFromExcel() throws MovieBookingAppException {
+		return new ResponseEntity<ResponseBody<List<String>>>(new ResponseBody<List<String>>(
+				movieTicketService.readingFromExcel(), null, "Reading from excel done", true), HttpStatus.OK);
+	}
+}
